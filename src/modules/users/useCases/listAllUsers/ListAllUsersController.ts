429e1e9eb5c55ad599e4d37cdeb711c3b7254778
kaneda96 <kanedaferrari@yahoo.com.br>
2023-01-23 21:17:30 -0300

    criando documentação

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -7,9 +7,10 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const { id } = request.body
+    const { id } = request.headers
+    const user_id = id[0];
     try {
-      const users = this.listAllUsersUseCase.execute({ user_id: id });
+      const users = this.listAllUsersUseCase.execute({ user_id });
       return response.status(200).send(users.map((user => user)));
     } catch (error) {
       return response.status(400).send({ error: error.message });
